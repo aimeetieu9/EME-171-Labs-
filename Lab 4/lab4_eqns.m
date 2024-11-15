fca6744057c67147b337929d6711801235bb9b04
aimeetieu9 <aatieu@ucdavis.edu>
2024-11-15 14:12:27 -0800

    1

--- Lab 4/lab4_eqns.m
+++ Lab 4/lab4_eqns.m
@@ -1,79 +1,43 @@
 function [ds, ext] = lab3_eqns(t, s)
 global rho g Ap P Cf L Ip At Ct Q0 d0 d1 d2;
-    
+     
     % State Variables (lab 4)
     p3 = s(1); 
     p9 = s(2); 
     q7 = s(3); 
     q13 = s(4);
 
-    % Effort Source (from lab 3)
+    % Effort Source (from lab 4)
     se1 = rho*g*d0;  
     se6 = rho*g*d1;  
     se12 = rho*g*d2; 
-    sf = QO_t; % changes with time MODIFYYYYYYYYYY
 
-    % Flow Source (lab 3)
-    
-    L = 0.5; % m, bump distance
-    %A = 0.16; % m, placeholder need to make max height before suspension deflection = 0.1m
+    % Flow Source (lab 4)
    
-    T1 = 0.5; %s, time when front tire hits first 
-    T2 = T1 + L/(2*vC); %s, front tire apex 1st 
-    T3 = T1 + L/vC; %s, front tire end first 
-    T4 = T1 + lWB/vC; %s, back tire start 1st 
-    T5 = T4 + L/(2*vC); %s, back tire apex 1st 
-    T6 = T4 + L/vC; %s, back tire end 1st & front tire start 2nd
-    T7 = T6 + L/(2*vC); %s, front tire apex 2nd
-    T8 = T6 + L/vC; %s, front tire end 2nd 
-    T9 = T6 + lWB/vC; %s, back tire start 2nd 
-    T10 = T9 + L/(2*vC); %s, back tire 2nd apex
-    T11 = T9 + L/vC; %s, back tire end 2nd
-  
-    Vamp = 2*A*vC/L;
+    T1 = 0; 
+    T2 = 2; %modify, chosen time 
+    T3 = T2 + 0.15;  
 
-   %setting vFI
-    if t < T1
-        vFI = 0;
-    elseif t >= T1 && t <= T2
-        vFI = Vamp;
+   %setting Q0
+    if t >= T1 && t <= T2
+        Q0 = 1.5;
     elseif t > T2 && t <= T3
-        vFI = -Vamp;
-    elseif t >= T6 && t <= T7
-        vFI = Vamp;
-    elseif t > T7 && t <= T8
-        vFI = -Vamp;
-    else
-        vFI = 0;
+        Q0 = 1.5 - (10*(t-T2));
+    elseif t > T3
+        Q0 = 0;
     end
-
-    %setting vRI
-     if t < T4
-        vRI = 0;
-    elseif t >= T4 && t <= T5
-        vRI = Vamp;
-    elseif t > T5 && t <= T6
-        vRI = -Vamp;
-    elseif t >= T9 && t <= T10
-        vRI = Vamp;
-    elseif t > T10 && t <= T11
-        vRI = -Vamp;
-    else
-        vRI = 0;
-    end
-
 
     % Equations of Motion (EOM) (lab 4)
     qdot7 = (p3/Ip) - (p9/Ip); 
     qdot13 = (p9/Ip) - Q0;
     pdot3 = (rho*g*d0) + (rho*g*d1) - (Cf* (p3/Ip) * abs(p3/Ip)) - (q7/Ct);
     pdot9 = (q7/Ct) + (rho*g*d2) + (q13/Ct) - (Cf*(p9/Ip)*abs(p9/Ip));
+    
 
 
     % External variables
-    ext(1) = vRI; 
-    ext(2) = vFI; 
+    ext(1) = Q0;
     
     % State derivatives (lab 4)
-    ds = [qdot7; qdot13; pdot3; pdot9];
+    ds = [pdot3; pdot9; qdot7; qdot13];
 end
--- Lab 4/lab4_master.m
+++ Lab 4/lab4_master.m
@@ -7,16 +7,18 @@ global rho g Ap P Cf L Ip At Ct Q0 d0 d1 d2;
 rho  = 1000; %kg/m^3 water rho 
 g = 9.81; %m/s^2 acceleration due to gravity
 Ap = 0.1; %m^2 area of pipe from reservoir to turbine
-P = rho *g*dh; %N/m^2 hydrostatic pressure... DIFFERENT dh BETWEEN NODES
+% P = rho * g *dh; %N/m^2 hydrostatic pressure... DIFFERENT dh BETWEEN NODES
 Cf = 49000; %kg/m^7 fluid pipe resistance constant
 L = 50; %m piper section lengths
-Ip = rho *L*Ap; %kg/m^4 fluid inertia of pipe sections
-At = ; %m^2 tank areas... FIND MAX TANK AREA BY SIMULATING DIFF VALUES OF At (increments of 0.05 m^2)
+Ip = rho *L/Ap; %kg/m^4 fluid inertia of pipe sections
+At = 1; %m^2 tank areas... FIND MIN TANK AREA BY SIMULATING DIFF VALUES OF At (increments of 0.05 m^2)
 Ct = At / (rho *g); %m^5/N fluid capacitance of each tank
 Q0 = 1.5; %given 
 d0 = 20;
 d1= 20; 
 d2 = 20; 
+h1max = 5 + d0 + d1; %m
+h2max = h1max + d2; %m
 
 % Initial conditions
 p9_0 = Q0 * Ip; 
@@ -25,3 +27,25 @@ q7_0 = Ct * ((rho*g*d0) + (rho*g*d1) - (Cf*Q0*abs(Q0)));
 q13_0 = Ct * ((q7_0/Ct) + (rho*g*d2) - (Cf*Q0*abs(Q0))); 
 initial = [p9_0, p3_0, q7_0, q13_0]; 
 
+
+tspan = linspace(0,200,5000);
+
+[t, s] = ode45(@lab4_eqns,tspan,initial);
+
+ext = zeros(length(t),1);
+ds = zeros(length(t),4);
+
+for i = 1:length(t)
+[ds(i,:) ext(i,:)] = lab4_eqns(t(i), s(i,:));
+end
+
+h1 = s(:,3) / At;
+h2 = s(:,4) / At;
+
+figure('Name','Heights h','NumberTitle','off','Color','white')
+plot(t, h1,'k',t, h2, 'r', t, h1max, 'b', t, h2max, 'g'), grid on
+%plot(t, ext(:,1), 'g'), grid on
+title('Suspension Deflection')
+ylabel('displacement (m)')
+xlabel('time (s)')
+legend('height 1', 'height 2', 'max height 1','max height 2')
