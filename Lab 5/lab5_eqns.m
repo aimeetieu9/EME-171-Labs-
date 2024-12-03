9b609ddabf4d1a125a1a5899e305f0a5260bd939
aimeetieu9 <aatieu@ucdavis.edu>
2024-12-03 13:37:09 -0800

    fixed

--- Lab 5/lab5_eqns.m
+++ Lab 5/lab5_eqns.m
@@ -14,9 +14,41 @@
 % end
 %% 
 % %% Part 2 
+function [ds, ext] = lab5_eqns(t,s)
+global Rw Lw Tm M btau R Gr Cr Cd rho Af Uin g vref dref uin kp ki
+%State variables
+pL = s(1);
+pM = s(2);
+dref= s(3);
+dact= s(4);
+
+% Approximation for signum function (From Part 1)
+sgn_approx = pM / (abs(pM) + 1e-5);
+
+
+T1 = 0; %Chosen Start Time 
+if t<0
+    vref = 0;
+else
+    vref = 1;
+end
+% Input Velocity (given) 
+uin = kp*(vref-pM/M) + ki*(dref-dact);
+
+% Equations of Motion (EOM) (lab 5)
+pLdot = uin - (pL*Rw)/Lw - (Gr*pM*Tm)/(R*M);
+pMdot = (Gr/R)*((Tm*pL/Lw) - (pM*btau/M)) - M*g*Cr*sgn_approx - 0.5*rho*Af*Cd*(pM/M)*(pM/M);
+d_refdot = vref;
+ddot = pM/M;
+
+ds = [pLdot; pMdot; d_refdot; ddot];
+ext=[vref];
+end
+
+%% Part 3
 % function [ds, ext] = lab5_eqns(t,s)
 % global Rw Lw Tm M btau R Gr Cr Cd rho Af Uin g vref dref uin kp ki
-% %State variables
+% %Extract the state variables
 % pL = s(1);
 % pM = s(2);
 % dref= s(3);
@@ -25,13 +57,8 @@
 % % Approximation for signum function (From Part 1)
 % sgn_approx = pM / (abs(pM) + 1e-5);
 % 
+% vref = LA92Oracle(t);
 % 
-% T1 = 0; %Chosen Start Time 
-% if t<0
-%     vref = 0;
-% else
-%     vref = 1;
-% end
 % % Input Velocity (given) 
 % uin = kp*(vref-pM/M) + ki*(dref-dact);
 % 
@@ -41,34 +68,7 @@
 % d_refdot = vref;
 % ddot = pM/M;
 % 
+% 
 % ds = [pLdot; pMdot; d_refdot; ddot];
-% ext=[vref];
+% ext = [vref];
 % end
-
-%% Part 3
-function [ds, ext] = lab5_eqns(t,s)
-global Rw Lw Tm M btau R Gr Cr Cd rho Af Uin g vref dref uin kp ki
-%Extract the state variables
-pL = s(1);
-pM = s(2);
-dref= s(3);
-dact= s(4);
-
-% Approximation for signum function (From Part 1)
-sgn_approx = pM / (abs(pM) + 1e-5);
-
-vref = LA92Oracle(t);
-
-% Input Velocity (given) 
-uin = kp*(vref-pM/M) + ki*(dref-dact);
-
-% Equations of Motion (EOM) (lab 5)
-pLdot = Uin - (pL*Rw)/Lw - (Gr*pM*Tm)/(R*M);
-pMdot = (Gr/R)*((Tm*pL/Lw) - (pM*btau/M)) - M*g*Cr*sgn_approx - 0.5*rho*Af*Cd*(pM/M)*(pM/M);
-d_refdot = vref;
-ddot = pM/M;
-
-
-ds = [pLdot; pMdot; d_refdot; ddot];
-ext = [vref];
-end
--- Lab 5/lab5_master.m
+++ Lab 5/lab5_master.m
@@ -1,46 +1,46 @@
 %% Part 1 
-clear all;
-close all;
-clc;
-global Rw Lw Tm M btau R Gr Cr g Cd rho Af Uin
-
-% Givens
-Rw = 0.3; % ohms
-Lw = 0.015; % henry
-Tm = 1.718; % Weber
-M = 2200; % kg
-btau = 0.05; % Nms/rad
-R = 0.2; % m
-Gr = 5; % ratio
-Cr = 0.006; % rolling resistance coefficient
-g = 9.81; % m/s^2
-Cd = 0.32; % drag coefficient
-rho = 1.21; % kg/m^3
-Af = 2.05; % m^2
-Uin = 100; %volts
-
-% Define initial conditions
-pL0 = 0;
-pM0 = 0;
-initial =[pL0,pM0];
-tspan=0:0.01:4; %simulating for 4 seconds with 0.01 time step  
-[t,s] = ode45(@lab5_eqns,tspan,initial);
-pL = s(:,1);
-pM = s(:,2);
-
-%ext = zeros(length(t),1);
-%ds = zeros(length(t),4);
-
-for i = 1:length(t)
-[ds(i,:)] = lab5_eqns(t(i), s(i,:));
-end
-
-
-figure; 
-plot(t, pM/M );
-xlabel('Time (s)');
-ylabel('Car velocity (m/s)');
-title(' Steady state speed ');
+% clear all;
+% close all;
+% clc;
+% global Rw Lw Tm M btau R Gr Cr g Cd rho Af Uin
+% 
+% % Givens
+% Rw = 0.3; % ohms
+% Lw = 0.015; % henry
+% Tm = 1.718; % Weber
+% M = 2200; % kg
+% btau = 0.05; % Nms/rad
+% R = 0.2; % m
+% Gr = 5; % ratio
+% Cr = 0.006; % rolling resistance coefficient
+% g = 9.81; % m/s^2
+% Cd = 0.32; % drag coefficient
+% rho = 1.21; % kg/m^3
+% Af = 2.05; % m^2
+% Uin = 100; %volts
+% 
+% % Define initial conditions
+% pL0 = 0;
+% pM0 = 0;
+% initial =[pL0,pM0];
+% tspan=0:0.01:4; %simulating for 4 seconds with 0.01 time step  
+% [t,s] = ode45(@lab5_eqns,tspan,initial);
+% pL = s(:,1);
+% pM = s(:,2);
+% 
+% %ext = zeros(length(t),1);
+% %ds = zeros(length(t),4);
+% 
+% for i = 1:length(t)
+% [ds(i,:)] = lab5_eqns(t(i), s(i,:));
+% end
+% 
+% 
+% figure; 
+% plot(t, pM/M );
+% xlabel('Time (s)');
+% ylabel('Car velocity (m/s)');
+% title(' Steady state speed ');
 
 %% Part 2
 clear all
@@ -70,8 +70,8 @@ ki = 300; %new to part 2
 pL0 = 0;
 pM0 = 0;
 dref0 = 0; %new to part 2 
-d0=0; %new to part 2 
-initial =[pL0; pM0; dref0; d0]; %new vars to part 2 
+dact0=0; %new to part 2 
+initial =[pL0; pM0; dref0; dact0]; %new vars to part 2 
 tspan =0:0.01:2; % different step for part 2 
 [t,s] = ode45(@lab5_eqns,tspan,initial);
 pL = s(:,1);
@@ -92,55 +92,55 @@ hold on
 plot (t,vref);
 
 %% Part 3
-clear all
-close all
-clc
-
-% Givens
-global Rw Lw Tm M btau R Gr Cr g Cd rho Af Uin vref dref uin kp ki
-Rw = 0.3; % ohms
-Lw = 0.015; % henry
-Tm = 1.718; % Weber
-M = 2200; % kg
-btau = 0.05; % Nms/rad
-R = 0.2; % m
-Gr = 5; % ratio
-Cr = 0.006; % rolling resistance coefficient
-g = 9.81; % m/s^2
-Cd = 0.32; % drag coefficient
-rho = 1.21; % kg/m^3
-Af = 2.05; % m^2
-Uin = 100; %volts
-
-kp = 200;
-ki = 300;
-%Initial conditions
-pL0 = 0;
-pM0 = 0;
-dref0 = 0; 
-d0=0; 
-initial =[pL0; pM0; dref0; d0]; 
-tspan =0:0.01:300;
-[t,s] = ode45(@lab5_eqns,tspan,initial);
-pL = s(:,1);
-pM = s(:,2);
-dref= s(:,3); 
-dact= s(:,4);
-vref=zeros(length(t),1);
-
-for i=1:length(t)
-    vref(i) = LA92Oracle(t(i));
-end
-
-for i =1:length(t)
-    [ds(i,:), ext(i,:)] = lab5_eqns(t(i),s(i,:)); 
-end
-vref= ext(:,1);
-
-figure; 
-plot(t, pM/M)
-xlabel('Time(s)');
-ylabel('Velocity');
-title( 'Testing');
-hold on
-plot (t,vref);
+% clear all
+% close all
+% clc
+% 
+% % Givens
+% global Rw Lw Tm M btau R Gr Cr g Cd rho Af Uin vref dref uin kp ki
+% Rw = 0.3; % ohms
+% Lw = 0.015; % henry
+% Tm = 1.718; % Weber
+% M = 2200; % kg
+% btau = 0.05; % Nms/rad
+% R = 0.2; % m
+% Gr = 5; % ratio
+% Cr = 0.006; % rolling resistance coefficient
+% g = 9.81; % m/s^2
+% Cd = 0.32; % drag coefficient
+% rho = 1.21; % kg/m^3
+% Af = 2.05; % m^2
+% Uin = 100; %volts
+% 
+% kp = 200;
+% ki = 300;
+% %Initial conditions
+% pL0 = 0;
+% pM0 = 0;
+% dref0 = 0; 
+% d0=0; 
+% initial =[pL0; pM0; dref0; d0]; 
+% tspan =0:0.01:300;
+% [t,s] = ode45(@lab5_eqns,tspan,initial);
+% pL = s(:,1);
+% pM = s(:,2);
+% dref= s(:,3); 
+% dact= s(:,4);
+% vref=zeros(length(t),1);
+% 
+% for i=1:length(t)
+%     vref(i) = LA92Oracle(t(i));
+% end
+% 
+% for i =1:length(t)
+%     [ds(i,:), ext(i,:)] = lab5_eqns(t(i),s(i,:)); 
+% end
+% vref= ext(:,1);
+% 
+% figure; 
+% plot(t, pM/M)
+% xlabel('Time(s)');
+% ylabel('Velocity');
+% title( 'Testing');
+% hold on
+% plot (t,vref);
